85da35874af6c7b2dffbbd26c1ece392de68d3b4
MIkhail Karpenko <krosh347@gmail.com>
2018-10-13 00:13:43 +0500

    first commit

--- src/main/scala/fintech/homework03/PrefixTree.scala
+++ src/main/scala/fintech/homework03/PrefixTree.scala
@@ -1,5 +1,7 @@
 package fintech.homework03
 
+//import scala.collection.mutable.{Map}
+
 
 // Реализовать интерфейс PrefixTree
 // Интерфейс позволяет складывать объекты произвольного класса V по заданному "пути" Seq[K] в дерево
@@ -12,4 +14,36 @@ trait PrefixTree[K, +V] {
 
   def sub(path: Seq[K]): PrefixTree[K, V]
   def get: V
+}
+
+class MPrefixTree [K, V] private (value: Option[V], transitions: Map[K, PrefixTree[K, V]])
+  extends PrefixTree[K, V]{
+
+  def this(){
+    this(None: Option[V], Map[K, MPrefixTree[K, V]]())
+  }
+
+  override def put[U >: V](path: Seq[K], value: U): PrefixTree[K, U] = {
+    if (path.isEmpty)
+      return new MPrefixTree[K, U](Option(value), this.transitions)
+    else{
+      if (!transitions.contains(path.head)){
+        return new MPrefixTree[K, U](this.value,
+          transitions + (path.head -> new MPrefixTree[K, U]().put(path.tail, value)))
+      }
+      return new MPrefixTree[K, U](
+        this.value,
+        transitions + (path.head -> transitions(path.head).put(path.tail, value)))
+    }
+  }
+
+  override def sub(path: Seq[K]): PrefixTree[K, V] = {
+    if (path.isEmpty)
+      return this
+    if (!transitions.contains(path.head))
+      return new MPrefixTree[K, V]()
+    transitions(path.head).sub(path.tail)
+  }
+
+  override def get: V = value.getOrElse(throw new NoSuchElementException)
 }
--- src/test/scala/fintech/homework03/PrefixTreeSpec.scala
+++ src/test/scala/fintech/homework03/PrefixTreeSpec.scala
@@ -3,7 +3,7 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class PrefixTreeSpec extends FlatSpec with Matchers {
   it should "work well with strings" in {
-    val tree: PrefixTree[Char, Int] = ???
+    val tree: PrefixTree[Char, Int] = new MPrefixTree[Char, Int]()
 
     val with42: PrefixTree[Char, Int] = tree.put("abcd", 42)
     with42.sub("ab").sub("cd").get should be (42)
@@ -13,5 +13,43 @@ class PrefixTreeSpec extends FlatSpec with Matchers {
     withDouble.sub("ab").sub("cde").get should be (13.0)
   }
 
-  it should "???"
+  it should "work with [Char, String]" in {
+    val tree: PrefixTree[Char, String] = new MPrefixTree[Char, String]()
+
+    val mTree: PrefixTree[Char, String] = tree
+      .put("to", "to")
+      .put("t", "o")
+      .put("ten", "ten").put("ted", "ted").put("inn", "inn")
+    mTree.sub("ten").get should be ("ten")
+    mTree.sub("to").get should be ("to")
+    mTree.sub("t").get should be ("o")
+    mTree.sub("inn").get should be ("inn")
+    mTree.sub("inn").get should be ("inn")
+    an [NoSuchElementException] should be thrownBy mTree.sub("te").get
+  }
+
+  it should "work with lists" in {
+    val tree: PrefixTree[Char, Int] = new MPrefixTree[Char, Int]()
+
+    val mTree: PrefixTree[Char, Int] = tree
+      .put(List('a', 'b', 'c', 'd'), 1)
+      .put(List('a', 'b', 'e'), 2)
+      .put(List('e', 'f'),3)
+      .put(List('a', 'b'), 4)
+
+    mTree.sub(List('a', 'b', 'c', 'd')).get should be (1)
+    mTree.sub(List('a', 'b', 'e')).get should be (2)
+    mTree.sub(List('e', 'f')).get should be (3)
+    //and with string too
+    mTree.sub("ab").get should be (4)
+    an [NoSuchElementException] should be thrownBy mTree.sub("aaaa").get
+
+    an [NoSuchElementException] should be thrownBy mTree.sub(List('a')).get
+  }
+
+  it should "work with empty string" in {
+    val tree: PrefixTree[Char, Int] = new MPrefixTree[Char, Int]().put("", 100)
+
+    tree.sub("").get should be (100)
+  }
 }
